80d08cee58012b9b03b5414f07999f2964acc35b
Davo Smith <git@davosmith.co.uk>
2012-09-19 22:24:20 +0100

    Split plugins into 3 separate repos

--- version.php
+++ version.php
@@ -1,8 +1,10 @@
 <?php
 
-$plugin->version  = 2012080600;
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version  = 2012091900;
 $plugin->cron     = 0;
 $plugin->requires = 2010112400; // Moodle 2.0+
-$plugin->release  = '2.x (Build: 2012080600)';
+$plugin->release  = '2.x (Build: 2012091900)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array('mod_checklist' => 2010041800); // Must have checklist activity module installed
