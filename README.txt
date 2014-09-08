b844fc2146baf82441e10219ae4dc23aa3a2e5f5
Davo Smith <git@davosmith.co.uk>
2014-09-08 19:59:15 +0100

    Correctly match up the checklist activity after backup & restore

--- README.txt
+++ README.txt
@@ -5,7 +5,8 @@ http://moodle.org/plugins/view.php?plugin=mod_checklist
 
 ==Changes==
 
-* 2013-19-11 - Moodle 2.6 compatibility fixes
+* 2014-09-08 - Match up to new checklist activity after backup & restore
+* 2013-11-19 - Moodle 2.6 compatibility fixes
 * 2012-12-07 - Moodle 2.4 compatibility fixes
 * 2012-11-27 - Fixed problems with no students appearing in certain situations
 * 2012-09-22 - Fixed bug in group selection when user is not a member of any groups
--- version.php
+++ version.php
@@ -2,9 +2,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2013111900;
+$plugin->version  = 2014090800;
 $plugin->cron     = 0;
 $plugin->requires = 2010112400; // Moodle 2.0+
-$plugin->release  = '2.x (Build: 2013111900)';
+$plugin->release  = '2.x (Build: 2014090800)';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->dependencies = array('mod_checklist' => 2010041800); // Must have checklist activity module installed
+$plugin->dependencies = array('mod_checklist' => 2010041800); // Must have checklist activity module installed.
