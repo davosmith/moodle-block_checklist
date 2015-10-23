2c8e7175b248b7a75da5ed98d99ced7dc22c989e
Davo Smith <git@davosmith.co.uk>
2015-10-23 20:14:00 +0100

    Fix missing 'component' in version.php

--- version.php
+++ version.php
@@ -2,9 +2,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2015010100;
-$plugin->cron     = 0;
+$plugin->version = 2015102300;
+$plugin->cron = 0;
 $plugin->requires = 2010112400; // Moodle 2.0+
-$plugin->release  = '2.x (Build: 2015010100)';
+$plugin->release = '2.x (Build: 2015010100)';
+$plugin->component = 'block_checklist';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array('mod_checklist' => 2010041800); // Must have checklist activity module installed.
