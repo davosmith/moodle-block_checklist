d34327453a78d50b130fe8db2fcf491974e4b09d
Davo Smith <davo.smith@synergy-learning.com>
2026-04-24 15:28:39 +0100

    Update README and version

--- version.php
+++ version.php
@@ -24,10 +24,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025101800;
+$plugin->version = 2026042400;
 $plugin->requires = 2022112800; // Moodle 4.1.0.
-$plugin->release = '4.1.0.0';
+$plugin->release = '4.1.0.1';
 $plugin->component = 'block_checklist';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = ['mod_checklist' => 2019061900]; // Must have checklist activity module installed.
-$plugin->supported = [401, 501];
+$plugin->supported = [401, 502];
